67730c234b7562239ab20f45bd1e8e775897a175
Alex Pentland <apentlander@gmail.com>
2013-07-11 17:04:48 -0400

    Fixed the trim function
    The actiwatch and dimsimeter now will have the same number of values

---
+++ trimData.m
@@ -0,0 +1,15 @@
+function [timeOut, varargout] = trimData(timeIn, startTime, stopTime, cropStart, cropEnd, varargin)
+	% Find indices within specified range
+    stopTime = stopTime + 1.15741e-8;
+	if (~isnan(cropStart))
+		idx = timeIn >= startTime & timeIn <= stopTime & (time <= cropStart | time >= cropEnd);
+	else
+		idx = timeIn >= startTime & timeIn <= stopTime;
+	end
+	% Trim time
+	timeOut = timeIn(idx);
+	% Trim other inputs
+	for i1 = 1:nargin-5
+		varargout{i1} = varargin{i1}(idx);
+	end
+end
