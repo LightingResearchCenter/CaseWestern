67c5b404511cb7d2c480108655f2717a6fcd17ad
Alex Pentland <apentlander@gmail.com>
2013-07-12 11:49:26 -0400

    Fixed equality bug
    Matlab wouldn't recognize two values were equal so 1 millisecond was
    subtracted from the start to ensure it is within the range

--- trimData.m
+++ trimData.m
@@ -1,6 +1,8 @@
 function [timeOut, varargout] = trimData(timeIn, startTime, stopTime, cropStart, cropEnd, varargin)
 	% Find indices within specified range
-    stopTime = stopTime + 1.15741e-8;
+	one_milli = 1.15741e-8;
+	startTime = startTime - one_milli;
+    stopTime = stopTime + one_milli;
 	if (~isnan(cropStart))
 		idx = timeIn >= startTime & timeIn <= stopTime & (timeIn <= cropStart | timeIn >= cropEnd);
 	else
