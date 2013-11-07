821fbfac35919e7925e9409ef154efcdcfe4b554
Geoffrey Jones <jonesg5@rpi.edu>
2013-11-07 11:24:17 -0500

    Updated sleep algorithm

--- sleepAnalysis/AnalyzeFile.m
+++ sleepAnalysis/AnalyzeFile.m
@@ -1,17 +1,8 @@
 function [Subject,Phase,ActualSleep,ActualSleepPercent,ActualWake,...
     ActualWakePercent,SleepEfficiency,Latency,SleepBouts,WakeBouts,...
-    MeanSleepBout,MeanWakeBout] = AnalyzeFile(Time,Activity,BedTime,GetUpTime,Subject,Phase,Threshold,f)
+    MeanSleepBout,MeanWakeBout] = AnalyzeFile(Time,Activity,BedTime,GetUpTime,Subject,Phase)
+
 Days = length(BedTime);
-
-% Preallocate AnalysisStart and AnalysisEnd
-AnalysisStart = zeros(Days,1);
-AnalysisEnd = zeros(Days,1);
-
-% Set AnalysisStart and AnalysisEnd for each day
-for i = 1:Days
-    AnalysisStart(i) = addtodate(BedTime(i),-3,'minute');
-    AnalysisEnd(i) = addtodate(GetUpTime(i),3,'minute');
-end
 
 % Preallocate sleep parameters
 SleepStart = zeros(Days,1);
@@ -31,8 +22,7 @@ for i = 1:Days
 [SleepStart(i),SleepEnd(i),ActualSleep(i),ActualSleepPercent(i),...
     ActualWake(i),ActualWakePercent(i),SleepEfficiency(i),Latency(i),...
     SleepBouts(i),WakeBouts(i),MeanSleepBout(i),MeanWakeBout(i)] = ...
-    CalcSleepParams(Activity,Time,AnalysisStart(i),AnalysisEnd(i),...
-    BedTime(i),GetUpTime(i),Threshold,f);
+    CalcSleepParams(Activity,Time,BedTime(i),GetUpTime(i));
 end
 
 end
--- sleepAnalysis/CalcSleepParams.m
+++ sleepAnalysis/CalcSleepParams.m
@@ -1,109 +1,89 @@
 function [SleepStart,SleepEnd,ActualSleep,ActualSleepPercent,...
     ActualWake,ActualWakePercent,SleepEfficiency,Latency,SleepBouts,...
     WakeBouts,MeanSleepBout,MeanWakeBout] = ...
-    CalcSleepParams(Activity,Time,AnalysisStart,AnalysisEnd,...
-    BedTime,GetUpTime,Threshold,f)
+    CalcSleepParams(Activity,Time,bedTime,wakeTime)
 %CALCSLEEPPARAMS Calculate sleep parameters using Actiware method
 %   Values and calculations are taken from Avtiware-Sleep
 %   Version 3.4 documentation Appendix: A-1 Actiwatch Algorithm
 
 % Trim Activity and Time to times within the Start and End of the analysis
 % period
-i = find(Time > AnalysisStart,1);
-if i >= length(Time);
-    error(['index = ',num2str(i)]);
-elseif i > 1
-    Activity(1:i) = [];
-    Time(1:i) = [];
+buffer = 5/(60*24); % 5 minute buffer
+idx = Time >= (bedTime - buffer) & Time <= (wakeTime + buffer);
+Time = Time(idx);
+try
+    Activity = gaussian(Activity(idx),4);
+catch
 end
-i = find(Time < AnalysisEnd,1,'last');
-if i == 1
-    error(['index = ',num2str(i)]);
-elseif i < length(Time)
-    Activity(i+1:end) = [];
-    Time(i+1:end) = [];
-end
-clear i
 
 % Find the sleep state
-SleepState = FindSleepState(Activity,Threshold,f);
+sleepState = FindSleepState(Activity,'auto',3);
 
-Epoch = etime(datevec(Time(2)),datevec(Time(1))); % Find epoch length
-n = ceil(300/Epoch); % Number of points in a 5 minute interval
+% Find sleep state in a 10 minute window
+Epoch = etime(datevec(Time(2)),datevec(Time(1))); % Find epoch length in seconds
+n10 = ceil(600/Epoch); % Number of points in a 10 minute interval
+n5 = floor((n10)/2);
+notSleepState = ~sleepState;
+activeState10 = notSleepState;
+for i1 = -n5:n5
+    activeState10 = activeState10 + circshift(notSleepState,i1);
+end
+sleepState10 = activeState10 <= 1;
+Time2 = Time;
+
+% Remove first and last 10 minutes
+last = length(Time2);
+Time2((last-n5):last) = [];
+sleepState10((last-n5):last) = [];
+Time2(1:n5) = [];
+sleepState10(1:n5) = [];
 
 % Find Sleep Start
-i = 1+n;
-while i <= length(SleepState)-n
-    if length(find(SleepState(i-n:i+n)==0)) == 1
-        SleepStartIndex = i;
-        SleepStart = Time(i);
-        break
-    else
-        i = i+1;
-    end
-end
-
-% Set Sleep Start to Bed Time if it was not found
-if exist('SleepStart','var') == 0
-    SleepStart = BedTime;
-    SleepStartIndex = find(Time > BedTime,1);
-end
+idxStart = find(sleepState10,true,'first');
+SleepStart = Time2(idxStart);
 
 % Find Sleep End
-j = length(Time)-n;
-while j > n+1
-    if length(find(SleepState(j-n:j+n)==0)) == 1
-        SleepEndIndex = j;
-        SleepEnd = Time(j);
-        break
-    else
-        j = j-1;
+idxEnd = find(sleepState10,true,'last');
+SleepEnd = Time2(idxEnd);
+
+%% Calculate the parameters
+inBedSleeping = Time >= SleepStart & Time <= SleepEnd;
+% Calculate Actual Sleep Time in minutes
+ActualSleep = sum(sleepState(inBedSleeping))*Epoch/60;
+% Calculate Actual Wake Time in minutes
+ActualWake = sum(sleepState(inBedSleeping)==0)*Epoch/60;
+% Calculate Assumed Sleep in minutes
+AssumedSleep = ActualSleep + ActualWake;
+% Calculate Actual Sleep Time Percentage
+ActualSleepPercent = ActualSleep/AssumedSleep;
+% Calculate Actual Wake Time Percentage
+ActualWakePercent = ActualWake/AssumedSleep;
+% Calculate Sleep Efficiency in minutes
+TimeInBed = etime(datevec(wakeTime),datevec(bedTime))/60;
+SleepEfficiency = ActualSleep/TimeInBed;
+% Calculate Sleep Latency in minutes
+Latency = etime(datevec(SleepStart),datevec(bedTime))/60;
+% Find Sleep Bouts and Wake Bouts
+SleepBouts = 0;
+WakeBouts = 0;
+for i = 2:length(sleepState)
+    if sleepState(i) == 1 && sleepState(i-1) == 0
+        SleepBouts = SleepBouts+1;
+    end
+    if sleepState(i) == 0 && sleepState(i-1) == 1
+        WakeBouts = WakeBouts+1;
     end
 end
-% Returns zero values for all parameters if data is unusable
-if exist('SleepEnd','var') == 0 || AnalysisStart > datenum(2013,1,1)
-    SleepStart = datenum(0,1,1);
-    SleepEnd = datenum(0,1,1);
-    ActualSleep = 0;
-    ActualSleepPercent = 0;
-    ActualWake = 0;
-    ActualWakePercent = 0;
-    SleepEfficiency = 0;
-    Latency = 0;
-    SleepBouts = 0;
-    WakeBouts = 0;
+% Calculate Mean Sleep Bout Time in minutes
+if SleepBouts == 0
     MeanSleepBout = 0;
-    MeanWakeBout = 0;
-else % Otherwise calculate the parameters
-    % Calculate Assumed Sleep in minutes
-    AssumedSleep = etime(datevec(Time(SleepEndIndex)),datevec(Time(SleepStartIndex)))/60;
-    % Calculate Actual Sleep Time in minutes
-    ActualSleep = sum(SleepState(SleepStartIndex:SleepEndIndex))*Epoch/60;
-    % Calculate Actual Sleep Time Percentage
-    ActualSleepPercent = ActualSleep*100/AssumedSleep;
-    % Calculate Actual Wake Time in minutes
-    ActualWake = length(find(SleepState(SleepStartIndex:SleepEndIndex)==0))*Epoch/60;
-    % Calculate Actual Wake Time Percentage
-    ActualWakePercent = ActualWake*100/AssumedSleep;
-    % Calculate Sleep Efficiency in minutes
-    TimeInBed = etime(datevec(GetUpTime),datevec(BedTime))/60;
-    SleepEfficiency = ActualSleep*100/TimeInBed;
-    % Calculate Sleep Latency in minutes
-    Latency = etime(datevec(SleepStart),datevec(BedTime))/60;
-    % Find Sleep Bouts and Wake Bouts
-    SleepBouts = 0;
-    WakeBouts = 0;
-    for i = 2:length(SleepState)
-        if SleepState(i) == 1 && SleepState(i-1) == 0
-            SleepBouts = SleepBouts+1;
-        end
-        if SleepState(i) == 0 && SleepState(i-1) == 1
-            WakeBouts = WakeBouts+1;
-        end
-    end
-    % Calculate Mean Sleep Bout Time in minutes
+else
     MeanSleepBout = ActualSleep/SleepBouts;
-    % Claculate Mean Wake Bout Time in minutes
+end
+% Claculate Mean Wake Bout Time in minutes
+if WakeBouts == 0
+    MeanWakeBout = 0;
+else
     MeanWakeBout = ActualWake/WakeBouts;
 end
 
